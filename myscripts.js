163c64c1aabb1c9673161d42d7873980347d61db
codydegen <codydegen@gmail.com>
2019-10-22 20:45:23 -0400

    Add Black and Rainbow functions

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,24 @@
 const container = document.querySelector('#container');
 
 const grid = document.createElement('div');
-let gridSize = getComputedStyle(container)//.grid-template-columns
+//let gridSize = getComputedStyle(container)//.grid-template-columns
+let fillMethod = "black";
 
-//grid.classList.add('box');
-//grid.textContent = 'test';
-//const grid2 = document.createElement('div');
-//grid2.classList.add('box');
-//grid2.textContent = 'test2';
-//let tempElement = document.createElement('div');
 populateGrid(16);
 const clearbtn = document.querySelector('#clear');
 clearbtn.addEventListener('click', resetGrid);
+const colorbtn = document.querySelectorAll(".color");
+colorbtn.forEach((button) => button.addEventListener('click', changeFillMethod));
+//colorbtn.addEventListener;
+
+function changeFillMethod(e){
+    const colorbtn = document.querySelectorAll(".color");
+    colorbtn.forEach((button) => button.classList.remove('selected'));
+    this.classList.add('selected');
+    
+    console.log(e.target.firstChild.data);
+    return e.target.firstChild.data;
+}
 
 function resetGrid(){//remove all elements from grid so it can be resized, ask for new number of elements
     const container = document.querySelector('#container');  
@@ -31,7 +38,7 @@ function resetGrid(){//remove all elements from grid so it can be resized, ask f
 
 function populateGrid(gridSize){//create grid with size of input
     const container = document.querySelector('#container');
-    container.setAttribute('style','grid-template: repeat('+gridSize+',1fr) / repeat('+gridSize+',1fr);');
+    container.setAttribute('style','grid-template: repeat('+gridSize+',1fr) / repeat('+gridSize+',1fr); background: hsl(360,50%,50%);');
     for (let i = 1; i <=gridSize*gridSize;i++){
         //console.log(i);
         let tempElement = document.createElement('div');
@@ -42,10 +49,22 @@ function populateGrid(gridSize){//create grid with size of input
 
     }
     const boxes = document.querySelectorAll('.box');
-    boxes.forEach(square => square.addEventListener('mouseenter', mouseOver));
+    boxes.forEach((square) => square.addEventListener('mouseenter', mouseOver));
 }
 function mouseOver(e) {
-    this.classList.add('filled');
+    const fillMethod = document.querySelector(".selected").innerText;
+    if (fillMethod === 'Black'){
+        e.target.style.cssText = "background-color: hsl(0,0%,0%)";
+    }else if (fillMethod === 'Random'){
+        e.target.style.cssText = "background-color: hsl("+Math.random()*360+","+Math.random()*100+"%,"+Math.random()*100+"%)";
+    }else if (fillMethod === 'Gradient'){
+        console.log(e);
+        //if (console.logconsole.log(e.fromElement.style.backgroundColor);
+        //if (e.bgColor === ""){
+         //   e.bgColor
+        //}
+    }
+    //this.classList.add('filled');
 };
 
 
