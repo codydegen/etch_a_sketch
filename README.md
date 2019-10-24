cd21f2629ebb37d49acb244dbffd5c08dff79aa7
codydegen <codydegen@gmail.com>
2019-10-23 23:36:00 -0400

    Add Smooth and Propagate Functions, Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# etch_a_sketch
-Create Etch-a-Sketch as described by The Odin Project
+# Etch A Sketch
+Create an Etch-A-Sketch-esque tool using HTML, CSS, and JS.
+
+## General info
+This project was completed following The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project).  The purpose was to help teach myself basic DOM manipulation skills using JavaScript.
+
+## Screenshot
+![Example screenshot](/images/example.png)
+
+## Technologies
+* HTML
+* CSS
+* Javascript
+
+## Setup
+The live version of the website can be seen [here](https://codydegen.github.io/etch_a_sketch/).
+
+## Status
+The project is finished for now.  If possible in the future I'd like to go back and improve the HTML and CSS formatting but that's low priority for now.
+
+## 
--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,62 @@
-const container = document.querySelector('#container');
+//const container = document.querySelector('#container');
+//const grid = document.createElement('div');
 
-const grid = document.createElement('div');
-//let gridSize = getComputedStyle(container)//.grid-template-columns
-let fillMethod = "black";
-
+// populate grid to begin with, with 16x16 square
 populateGrid(16);
-const clearbtn = document.querySelector('#clear');
-clearbtn.addEventListener('click', resetGrid);
+
+// add event listener for each of the different methods of filling in boxes
 const colorbtn = document.querySelectorAll(".color");
 colorbtn.forEach((button) => button.addEventListener('click', changeFillMethod));
 
+// add event listener for reset grid button
+const clearbtn = document.querySelector('#clear');
+clearbtn.addEventListener('click', resetGrid);
 
-function changeFillMethod(e){
-    const colorbtn = document.querySelectorAll(".color");
-    colorbtn.forEach((button) => button.classList.remove('selected'));
-    this.classList.add('selected');
-    
-    console.log(e.target.firstChild.data);
-    return e.target.firstChild.data;
+// At event listener for smooth button
+const smooth = document.querySelector('#smooth');
+smooth.addEventListener('click', smoothToggle);
+
+// At event listener for propagate button
+const propagate = document.querySelector('#propagate');
+propagate.addEventListener('click', propagateToggle);
+
+//create grid with size of input
+function populateGrid(gridSize){
+    const container = document.querySelector('#container');
+    container.setAttribute('style','grid-template: repeat('+gridSize+',1fr) / repeat('+gridSize+',1fr);')
+    for (let i = 1; i <=gridSize; i++){
+        for (let j = 1; j <= gridSize; j++){
+
+            // Create temporary element with box class and class of x-y Location.  Set background color to white.
+            let tempElement = document.createElement('div');
+            tempElement.classList.add('box');
+            tempElement.classList.add('x'+j+'y'+i);
+            if (smoothCheck()) {
+                 tempElement.classList.add('smooth');
+                 console.log(' smooth ');
+            }else{
+                 tempElement.classList.remove('smooth');
+                 console.log(' not smooth ')
+             }
+             if (propagateCheck()) {
+                tempElement.classList.add('propagate');
+                tempElement.addEventListener('transitionend', propagatePattern);
+                console.log(' propagate ');
+           }else{
+                tempElement.classList.remove('propagate');
+                console.log(' not propagate ')
+            }
+            tempElement.setAttribute('style','background-color: rgb(255,255,255);');
+            container.appendChild(tempElement);
+        }
+    }
+    // Add event listener for each box that is a part of the grid
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((square) => square.addEventListener('mouseenter', mouseOver));
 }
 
-function resetGrid(){//remove all elements from grid so it can be resized, ask for new number of elements
+//remove all elements from grid so it can be resized, ask for new number of elements
+function resetGrid(){
     const container = document.querySelector('#container');  
     while (container.firstChild) {
         container.removeChild(container.firstChild);
@@ -36,62 +72,119 @@ function resetGrid(){//remove all elements from grid so it can be resized, ask f
     
 }
 
-function populateGrid(gridSize){//create grid with size of input
+//Change the fill method.  Fill method is determined by which button has the "selected" class attribute
+function changeFillMethod(e){
+    const colorbtn = document.querySelectorAll(".color");
+    colorbtn.forEach((button) => button.classList.remove('selected'));
+    this.classList.add('selected');
+    
+}
+
+// Add smooth toggle
+function smoothToggle(e){
+    const boxes = document.querySelectorAll(".box");
+    const smooth = document.querySelector("#smooth");
+    smooth.classList.toggle('off');
+    smooth.classList.toggle('on');
+    boxes.forEach((box) => box.classList.toggle('smooth'));
+}
+
+function smoothCheck(){
+     return document.querySelector("#smooth").classList.contains('on');
+ }
+ function propagateToggle(e){
+    const boxes = document.querySelectorAll(".box");
+    const propagate = document.querySelector("#propagate");
+    propagate.classList.toggle('off');
+    propagate.classList.toggle('on');
+    boxes.forEach((box) => box.classList.toggle('propagate'));
+    if(propagateCheck()){
+        boxes.forEach((box) => box.addEventListener('transitionend', propagatePattern));
+    }
+}
+
+function propagateCheck(){
+    return document.querySelector("#propagate").classList.contains('on');
+ }
+ function propagatePattern(e){
+    const fillMethod = document.querySelector(".selected").innerText;
+    const fillTarget = document.querySelector("."+e.target.classList[1]);
+    const thisY = parseInt(fillTarget.classList[1].split('y')[1]);
+    const thisX = parseInt(fillTarget.classList[1].split('y')[0].split('x')[1]);
     const container = document.querySelector('#container');
-    container.setAttribute('style','grid-template: repeat('+gridSize+',1fr) / repeat('+gridSize+',1fr); background-color: rgb(255,255,255);');
-    for (let i = 1; i <=gridSize; i++){
-        for (let j = 1; j <= gridSize; j++){
-            //console.log(i);
-            let tempElement = document.createElement('div');
-            //grid.createElement('div');
-            tempElement.classList.add('box');
-            tempElement.classList.add('x'+i+'y'+j);
-            tempElement.setAttribute('style','background-color: rgb(255,255,255);');
-            //tempElement.textContent = 'x'+i+'y'+j+'\n'+tempElement.getAttribute('style','background-color');
-            console.log(tempElement);
-            container.appendChild(tempElement);
+    let maxSize = Math.sqrt(container.childElementCount);
+    //console.log(ms);
+    //console.log(thisX, thisY);
+    for (let i = -1; i<=1; i++){
+        for (let j = -1; j<=1; j++){
+            //stuff
+            let tempX=thisX+i
+            let tempY=thisY+j
+            if ((i === 0 && j !== 0) || (i !== 0 && j === 0)){
+            if (tempX >=1 && tempX <= maxSize && tempY >= 1 && tempY <= maxSize){
+                //console.log(tempX);
+                //console.log(".x"+tempX+"y"+tempY);
+                let propElement = document.querySelector(".x"+tempX+"y"+tempY);
+                mouseOverDiv(propElement);
+            }
+        }
         }
     }
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((square) => square.addEventListener('mouseenter', mouseOver));
+
 }
+
+
+//Fill in boxes based on selected method
 function mouseOver(e) {
     const fillMethod = document.querySelector(".selected").innerText;
-    //console.log(e.target.classList[1]);
     const fillTarget = document.querySelector("."+e.target.classList[1]);
-    //console.log(fillTarget);
-    //console.log(fillTarget.getAttribute('style','background-color'));
+
     if (fillMethod === 'Black'){
         e.target.style.cssText = "background-color: rgb(0,0,0)";
     }else if (fillMethod === 'Random'){
         e.target.style.cssText = "background-color: hsl("+Math.random()*360+","+Math.random()*100+"%,"+Math.random()*100+"%)";
     }else if (fillMethod === 'Light Gradient'){
-        //console.log(e);
-        //if (console.logconsole.log(e.fromElement.style.backgroundColor);
-
-        // if (fillTarget.getAttribute('style','background-color') == ""){
-        // //if (e.fromElement.style.backgroundColor == ""){
-        // //    e.target.style.cssText = "background-color: rgb(250,250,250)";
-        //     alert("didn't expect this");
-
-        //     e.target.style.cssText = "background-color: rgb(250,250,250)";
-
-        // }else{
-            changeGradient(fillTarget, 25);
-        //}
+        changeGradient(fillTarget, 25);
     }else if (fillMethod === 'Dark Gradient'){
         changeGradient(fillTarget, -25);
     }
 
 };
+
+function mouseOverDiv(loc) {
+    const fillMethod = document.querySelector(".selected").innerText;
+    const fillTarget = document.querySelector("."+loc.classList[1]);
+
+    if (fillMethod === 'Black'){
+        loc.style.cssText = "background-color: rgb(0,0,0)";
+    }else if (fillMethod === 'Random'){
+        loc.style.cssText = "background-color: hsl("+Math.random()*360+","+Math.random()*100+"%,"+Math.random()*100+"%)";
+    }else if (fillMethod === 'Light Gradient'){
+        changeGradient(fillTarget, 25);
+    }else if (fillMethod === 'Dark Gradient'){
+        changeGradient(fillTarget, -25);
+    }
+
+};
+
+// For fill method for gradients, function was written to abstract the process
 function changeGradient(fillTarget, changeAmount){ 
     let colors = fillTarget.getAttribute('style','background-color').split(',');
-            //colors.forEach((element) => console.log(element))
             let red = Math.round(parseFloat(colors[0].split('(')[colors[0].split('(').length-1])+changeAmount);
             let green = Math.round(parseFloat(colors[1])+changeAmount);
             let blue = Math.round(parseFloat(colors[2].split(')')[0])+changeAmount);
-            //console.log("background-color: rgb("+red+", "+green+", "+blue+")");
+            red = checkColorLimits(red);
+            green = checkColorLimits(green);
+            blue = checkColorLimits(blue);
             fillTarget.setAttribute('style','background-color: rgb('+red+','+green+','+blue+');')
-            //e.target.style.cssText = "background-color: rgb("+red+", "+green+", "+blue+")";
 }
 
+function checkColorLimits(color){
+    if (color > 255){
+        return 255;
+    }else if (color < 0){
+        return 0;
+    }else {
+        return color;
+    }
+}
